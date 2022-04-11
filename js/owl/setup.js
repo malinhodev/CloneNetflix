bb73562e6cb1251a732a7c8a180cef75e863c40b
Marlon Ferreira <100293149+malinhodev@users.noreply.github.com>
2022-04-10 23:53:44 -0300

    alterações de minha autoria

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -14,3 +14,29 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+/****** scrolltop******/
+$(function(){
+    $('nav a').click(function(){
+        var href = $(this).attr('href');
+        var offSetTop = $(href).offset().top;
+
+        $('html,body').animate({'scrollTop':offSetTop});
+
+        return false;
+    })
+})
+/******fim scrolltop******/
+
+/******menu-mobile******/
+$(function(){
+    $('nav.mobile i').click(function(){
+        var el = $(this).parent().find('ul');
+        if(el.is(':visible') == false){
+         el.fadeIn();
+        }else{
+            el.fadeOut();
+        }
+    })
+})
+/******fim-menu-mobile******/
